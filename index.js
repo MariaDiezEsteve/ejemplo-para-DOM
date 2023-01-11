5307b7cd8383517523e0d7665e8e423c0d230c2a
MariaDiezEsteve <diezesteve@outlook.es>
2023-01-11 19:48:41 +0100

    solo falta classList

--- index.js
+++ index.js
@@ -28,9 +28,49 @@ paragraph.innerHTML = "Acabo de crear este párrafo";
 console.log(paragraph)
 console.log(paragraph.innerText)
 
+// 4. Estilos
 
-let color = document.getElementById("title1");
-color.style.color = "blue";
+        // Ejemplo 1:
+
+let changeColor = document.getElementById("title1");
+changeColor.style.color = "white";
+changeColor.style.backgroundColor = "black";
+
+let changeDiv = document.getElementById("estilos");
+changeDiv.style.borderRadius = '50%';
+changeDiv.style.display = 'flex';
+changeDiv.style.justifyContent = 'center';
+
+        // Ejemplo 2:
+
+let changePart1 = document.getElementById("part1")
+changePart1.style.setProperty('font-size', "3rem");
+
+let changePart2 = document.getElementById("part2")
+changePart2.style.setProperty('font-size', "2rem")
 
 
 
+// 5. Atributos
+
+let getImg = document.querySelector("img");
+
+        // 1. Ejemplo: src
+
+getImg.getAttribute("src");
+
+let newImg = getImg.setAttribute("src", "sincodigosinrazon.png")
+
+        // 2. Ejemplo: alt
+
+getImg.getAttribute("alt");
+
+let newAlt = getImg.setAttribute("alt", "sin codigo no hay razon")
+
+        // 3. Ejemplo: estilos
+
+let getStyle = document.getElementById("estilos")
+
+getStyle.getAttribute("style");
+
+let newStyle = getStyle.setAttribute("style", "text-align: center" )
