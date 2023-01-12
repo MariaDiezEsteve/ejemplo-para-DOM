8058b24f86d90a5d92543a9fe18e04d8e00b1ced
MariaDiezEsteve <diezesteve@outlook.es>
2023-01-12 23:08:32 +0100

    última versión

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ console.log("Hello World")
 
 // 1. Leer: innerText
 
-let titleDOM = document.querySelector("h1")
-// console.log(titleDOM)
-console.log(titleDOM.innerText)
+let title = document.querySelector("h1")
+console.log (title)
+console.log (title.innerText)
 
 
 // 2. Modificar: innerHTML
 
         // Ejemplo 1
 
-const title = document.getElementById("title2");
+const title2 = document.getElementById("title2");
+title2.innerHTML = "Fruta";
 
-title.innerHTML = "Fruit";
+const title3 = document.getElementById("title3");
+title3.innerHTML = "Verduras";
 
 
 // 3. Estilos
@@ -22,21 +24,21 @@ title.innerHTML = "Fruit";
         // Ejemplo 1:
 
 let changeColor = document.getElementById("title1");
-changeColor.style.color = "white";
-changeColor.style.backgroundColor = "black";
+// changeColor.style.color = "white";
+// changeColor.style.backgroundColor = "black";
 
  let changeDiv = document.getElementById("estilos");
- changeDiv.style.borderRadius = '50%';
- changeDiv.style.display = 'flex';
- changeDiv.style.justifyContent = 'center';
+//  changeDiv.style.borderRadius = '50%';
+//  changeDiv.style.display = 'flex';
+//  changeDiv.style.justifyContent = 'center';
 
         // Ejemplo 2:
 
 let changePart1 = document.getElementById("part1")
-changePart1.style.setProperty('font-size', "3rem");
+// changePart1.style.setProperty('font-size', "3rem");
 
 let changePart2 = document.getElementById("part2")
-changePart2.style.setProperty('font-size', "2rem")
+// changePart2.style.setProperty('font-size', "2rem")
 
 
 
@@ -49,13 +51,13 @@ let patata = document.querySelector("img");
 let imagen = patata.getAttribute("src");
 console.log(imagen)
 
-patata.setAttribute("src", "sincodigosinrazon.png")
+// patata.setAttribute("src", "sincodigosinrazon.png")
 
         // 2. Ejemplo: alt
 
 patata.getAttribute("alt");
 
-let newAlt = patata.setAttribute("alt", "sin codigo no hay razon")
+// patata.setAttribute("alt", "sin codigo no hay razon")
 
 
         // 3. Ejemplo: estilos
@@ -64,4 +66,19 @@ let kiwi = document.getElementById("estilos")
 
 kiwi.getAttribute("style");
         
-kiwi.setAttribute("style", "text-align: center" )
+// kiwi.setAttribute("style", "text-align: center" )
+
+
+// 5. classList
+
+let ejemplo = document.getElementById("lista-clases")
+
+ejemplo.classList.add("mystyle")
+
+
+console.log(ejemplo)
+
+// 6. value
+
+let valor = document.getElementById("nombre").value;
+console.log(valor)
